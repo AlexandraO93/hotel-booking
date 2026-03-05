41f3942675b2392356592334f0e962a961877272
AlexandraO93 <alexandra.olsson93@hotmail.com>
2026-03-05 10:11:25 +0100

    Added all pages jsx and content for aboutus and contact

---
+++ frontend/src/pages/Booking.jsx
@@ -0,0 +1,13 @@
+import RoslagenNavBar from "../components/Navbar";
+import hotelImage from "../assets/hotel.png";
+import BookingBar from "../components/BookingBar";
+
+export default function Booking() {
+    return (
+        <section>
+            <div>
+                
+            </div>
+        </section>
+    );
+}
---
+++ frontend/src/pages/Food-drinks.jsx
@@ -0,0 +1,12 @@
+import RoslagenNavBar from "../components/Navbar";
+import hotelImage from "../assets/hotel.png";
+import BookingBar from "../components/BookingBar";
+
+export default function FoodDrinks() {
+    return (
+        <section>
+            <div>
+            </div>
+        </section>
+    );
+}
