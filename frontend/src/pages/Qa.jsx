bf23b58c460a9df14e3c941872af495c40e5ccb6
AlexandraO93 <alexandra.olsson93@hotmail.com>
2026-03-19 17:09:27 +0100

    Fix: questions into mapped list

--- frontend/src/pages/Qa.jsx
+++ frontend/src/pages/Qa.jsx
@@ -20,42 +20,43 @@ export default function Qa() {
       answer: "Avbokning upp till max 4 rum ska ske senast 2 dagar innan ankomst (senast 15.00), samma gäller vid online bokning men då uttas en avboknings avgift på 100kr. För grupper/konferenser - vänligen kontakta oss via telefon på 070-123 45 67."
     },
     {
-      question: " När är in- och utcheckning?",
-      answer: "Incheckning 15:00. <br /> Utcheckning senast vid 11:00"
+      question: "När är in- och utcheckning?",
+      answer: "Incheckning 15:00. Utcheckning senast vid 11:00"
     },
     {
       question: "Ingår wifi?",
       answer: "Trådlöst internet finns på hotellet och är fritt att nyttja för samtliga hotellgäster."
     },
     {
-      question: "Vad gäller för av- och ombokning?",
-      answer: "Avbokning upp till max 4 rum ska ske senast 2 dagar innan ankomst (senast 15.00), samma gäller vid online bokning men då uttas en avboknings avgift på 100kr. För grupper/konferenser - vänligen kontakta oss via telefon på 070-123 45 67."
-    },{
-      question: "Vad gäller för av- och ombokning?",
-      answer: "Avbokning upp till max 4 rum ska ske senast 2 dagar innan ankomst (senast 15.00), samma gäller vid online bokning men då uttas en avboknings avgift på 100kr. För grupper/konferenser - vänligen kontakta oss via telefon på 070-123 45 67."
-    },{
-      question: "Vad gäller för av- och ombokning?",
-      answer: "Avbokning upp till max 4 rum ska ske senast 2 dagar innan ankomst (senast 15.00), samma gäller vid online bokning men då uttas en avboknings avgift på 100kr. För grupper/konferenser - vänligen kontakta oss via telefon på 070-123 45 67."
-    },{
-      question: "Vad gäller för av- och ombokning?",
-      answer: "Avbokning upp till max 4 rum ska ske senast 2 dagar innan ankomst (senast 15.00), samma gäller vid online bokning men då uttas en avboknings avgift på 100kr. För grupper/konferenser - vänligen kontakta oss via telefon på 070-123 45 67."
-    },{
-      question: "Vad gäller för av- och ombokning?",
-      answer: "Avbokning upp till max 4 rum ska ske senast 2 dagar innan ankomst (senast 15.00), samma gäller vid online bokning men då uttas en avboknings avgift på 100kr. För grupper/konferenser - vänligen kontakta oss via telefon på 070-123 45 67."
+      question: "Har ni roomservice?",
+      answer: "Hos oss kommer du/ni för att slappna av och må bra, självklart bistår vi er med roomservice om så önskas!"
+    },
+    {
+      question: "Har ni extrasäng att boka till rummet?",
+      answer: "Vi tillgodoser er med extrasäng om din rumskategori tillåter det."
+    },
+    {
+      question: "Har ni allergirum?",
+      answer: "Självklart! Men du måste kontakta receptionen om du vill boka ett allergirum. Går ej boka online."
+    },
+    {
+      question: "Måste man förskottsbetala en bokning?",
+      answer: "Samtliga bokningar som görs via vår onlinebokning måste förskottsbetalas, Bokningar som görs direkt till hotellet per telefon eller mail betalas vid ankomst direkt till hotellet."
+    },
+    {
+      question: "Kan man komma och bara äta frukost eller middag hos er?",
+      answer: "Alla är välkomna till oss, så självklart har du möjlighet att äta hos oss även fast du inte bor på hotellet."
     },
   ];
 
   function toggleQuestion(index) {
     setOpenIndex(prev => (prev === index ? null : index));
-    questions.map(question, index) => (
-      question.
-    )
-  };
+  }
 
   return (
     <div className="qa-page">
       <div
-        className="qa-img"
+        className={`qa-img ${heroLoaded ? "loaded" : ""}`}
         style={{
           backgroundImage: `linear-gradient(rgba(255, 255, 255, 0.0005), rgba(255, 255, 255, 0.0005)), url(${heroImage})`,
         }}
@@ -65,143 +66,21 @@ export default function Qa() {
         <h2 className="qa-main-headline">Frågor och Svar</h2>
 
         <section className="qa-content">
-          <span className="question-text">
-            <button
+          {questions.map((item,index) => (
+            <div key={index} className="qa-item">
+              <button
               className="qa-question"
-              onClick={toggle4}
-              aria-expanded={questionOpen4}
-            >
-              Vad gäller för av- och ombokning?
-            </button>
-            {questionOpen4 && (
-              <p className="qa-answer">
-                Avbokning upp till max 4 rum ska ske senast 2 dagar innan
-                ankomst (senast 15.00), samma gäller vid online bokning men då
-                uttas en avboknings avgift på 100kr. För grupper/konferenser -
-                vänligen kontakta oss via telefon på 070-123 45 67.
-              </p>
-            )}
-          </span>
-        </section>
-        <section className="qa-content">
-          <span className="question-text">
-            <button
-              className="qa-question"
-              onClick={toggle1}
-              aria-expanded={questionOpen1}
-            >
-              När är in- och utcheckning?
-            </button>
-            {questionOpen1 && (
-              <p className="qa-answer">
-                Incheckning 15:00. <br />
-                Utcheckning senast vid 11:00
-              </p>
-            )}
-          </span>
-        </section>
-        <section className="qa-content">
-          <span className="question-text">
-            <button
-              className="qa-question"
-              onClick={toggle2}
-              aria-expanded={questionOpen2}
-            >
-              Ingår wifi?
-            </button>
-            {questionOpen2 && (
-              <p className="qa-answer">
-                Trådlöst internet finns på hotellet och är fritt att nyttja för
-                samtliga hotellgäster.
-              </p>
-            )}
-          </span>
-        </section>
-        <section className="qa-content">
-          <span className="question-text">
-            <button
-              className="qa-question"
-              onClick={toggle3}
-              aria-expanded={questionOpen3}
-            >
-              Har ni roomservice?
-            </button>
-            {questionOpen3 && (
-              <p className="qa-answer">
-                Hos oss kommer du/ni för att slappna av och må bra, självklart
-                bistår vi er med roomservice om så önskas!
-              </p>
-            )}
-          </span>
-        </section>
-        <section className="qa-content">
-          <span className="question-text">
-            <button
-              className="qa-question"
-              onClick={toggle6}
-              aria-expanded={questionOpen6}
-            >
-              Har ni extrasäng att boka till rummet?
-            </button>
-            {questionOpen6 && (
-              <p className="qa-answer">
-                Vi tillgodoser er med extrasäng om din rumskategori tillåter
-                det.
-              </p>
-            )}
-          </span>
-        </section>
-        <section className="qa-content">
-          <span className="question-text">
-            <button
-              className="qa-question"
-              onClick={toggle8}
-              aria-expanded={questionOpen8}
-            >
-              Har ni allergirum?
-            </button>
-            {questionOpen8 && (
-              <p className="qa-answer">
-                Självklart! Men du måste kontakta receptionen om du vill boka
-                ett allergirum. Går ej boka online.
-              </p>
-            )}
-          </span>
-        </section>
-        <section className="qa-content">
-          <span className="question-text">
-            <button
-              className="qa-question"
-              onClick={toggle5}
-              aria-expanded={questionOpen5}
-            >
-              Måste man förskottsbetala en bokning?
-            </button>
-            {questionOpen5 && (
-              <p className="qa-answer">
-                Samtliga bokningar som görs via vår onlinebokning måste
-                förskottsbetalas, Bokningar som görs direkt till hotellet per
-                telefon eller mail betalas vid ankomst direkt till hotellet.
-              </p>
-            )}
-          </span>
-        </section>
-        <section className="qa-content">
-          <span className="question-text">
-            <button
-              className="qa-question"
-              onClick={toggle7}
-              aria-expanded={questionOpen7}
-            >
-              Kan man komma och bara äta frukost eller middag hos er?
-            </button>
-            {questionOpen7 && (
-              <p className="qa-answer">
-                Alla är välkomna till oss, så självklart har du möjlighet att
-                äta hos oss även fast du inte bor på hotellet.
-              </p>
-            )}
-          </span>
+              onClick={() => toggleQuestion(index)}
+              aria-expanded={openIndex === index}
+              >
+                {item.question}
+              </button>
+              
+              {openIndex === index && (
+                <p className="qa-answer">{item.answer}</p>
+              )}
+            </div>
+          ))}
         </section>
       </main>
     </div>
